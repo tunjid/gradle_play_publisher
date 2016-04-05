dc372428d480f238355c04a0061654c8ead0d87b
Tunjid Dahunsi <tjdah100@gmail.com>
2016-04-05 10:05:01 -0400

    Removed unneccessary imports.

--- src/main/groovy/de/triplet/gradle/play/PublishTask.groovy
+++ src/main/groovy/de/triplet/gradle/play/PublishTask.groovy
@@ -1,8 +1,6 @@
 package de.triplet.gradle.play
 
 import org.gradle.api.DefaultTask
-import org.gradle.api.Project
-import org.gradle.api.Task
 
 /**
  * A task that depends on a {@link PlayPublishListingTask} and {@link PlayPublishApkTask}
